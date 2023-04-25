f4c472d2c4782e28d1fb98ccef4f4fe08444f000
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-25 16:38:37 -0500

    implementacion paquete dio para peticiones http

--- lib/domain/datasources/movies_datasources.dart
+++ lib/domain/datasources/movies_datasources.dart
@@ -2,7 +2,7 @@
 
 import 'package:cinemapedia/domain/entities/movie_entity.dart';
 
-abstract class MovieDatasource {
+abstract class MoviesDatasource {
 
   Future<List<MovieEntity>>getNowPlaying({ int page = 1 });
 
---
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,30 @@
+
+
+
+
+
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
+import 'package:cinemapedia/domain/entities/movie_entity.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDatasource{
+
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Environment.theMovieDbKey,
+      'language': 'es-MX',
+    }
+  ));
+
+  @override
+  Future<List<MovieEntity>> getNowPlaying({int page = 1}) async {
+
+    final response = await dio.get('/movie/now_playing');
+    final List<MovieEntity> movies = [];
+    
+    return [];
+  }
+
+}

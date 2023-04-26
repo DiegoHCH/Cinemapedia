d713183d3d0e07cb3ae5409e1cee6859aa934ab1
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-25 19:41:25 -0500

    creacion del mapper movie

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,27 @@
+import 'package:cinemapedia/domain/entities/movie_entity.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+
+  static MovieEntity movieDBToEntity(MovieMovieDB movieDB) => MovieEntity(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath != '') 
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+        : 'https://studiolegalelisi.it/wp-content/uploads/2021/03/errore-404-1200x514.jpg',
+      genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath != '') 
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+        : 'https://studiolegalelisi.it/wp-content/uploads/2021/03/errore-404-1200x514.jpg',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount
+  );
+  
+}

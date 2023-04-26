73ed944193d21acf70b1997861f4132c670d8818
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-04-25 19:53:14 -0500

    movie repository implements

--- lib/domain/repositories/movies_repository.dart
+++ lib/domain/repositories/movies_repository.dart
@@ -1,7 +1,7 @@
 
 import 'package:cinemapedia/domain/entities/movie_entity.dart';
 
-abstract class MovieRepository {
+abstract class MoviesRepository {
 
   Future<List<MovieEntity>>getNowPlaying({ int page = 1 });
 
---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:cinemapedia/domain/entities/movie_entity.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+import '../../domain/datasources/movies_datasources.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource); 
+
+  @override
+  Future<List<MovieEntity>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}

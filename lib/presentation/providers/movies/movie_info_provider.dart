55c32a495758f69c9e216209406bbec4df6749e9
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-05-21 21:03:09 -0500

    Provider para manejo de cache local de las peliculas

---
+++ lib/presentation/providers/movies/movie_info_provider.dart
@@ -0,0 +1,33 @@
+
+
+import 'package:cinemapedia/domain/entities/movie_entity.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+
+final movieInfoProvider = StateNotifierProvider((ref) {
+  final moviesRepository = ref.watch(moviesRepositoryProvider);
+  return MovieMapNotifier(getMovie: moviesRepository.getMovieByID);
+});
+
+
+
+
+
+typedef GetMovieCallback = Future<MovieEntity>Function(String movieID);
+
+class MovieMapNotifier extends StateNotifier<Map<String,MovieEntity>> {
+
+  final GetMovieCallback getMovie;
+
+  MovieMapNotifier({required this.getMovie}): super({});
+
+  Future<void> loadMovie(String movieID) async {
+
+    if( state[movieID] != null ) return;
+
+    final movie = await getMovie( movieID );
+
+    state = { ...state, movieID: movie};
+  }
+}

566b3a457f6249113e64315c24309f9a778960f9
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-06-08 14:20:13 -0500

    Mostrar actores de peliculas

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -110,9 +110,65 @@ class _MovieDetails extends StatelessWidget {
             ),
           ),
 
-          const SizedBox( height: 100,)
+          _ActorsByMovie(movieID: (movie.id).toString()),
+
+          const SizedBox( height: 20,)
         
       ],
+    );
+  }
+}
+
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieID;
+
+  const _ActorsByMovie({required this.movieID});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch(actorsMovieProvider);
+
+    if(actorsByMovie[movieID] == null) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+    final actors = actorsByMovie[movieID]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (BuildContext context, int index) { 
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 135,
+                    fit: BoxFit.cover,),
+                ),
+                const SizedBox(height: 5,),
+                Text(actor.name, maxLines: 2,),
+                Text(
+                  actor.character ?? '',
+                  maxLines: 2,
+                  style: const TextStyle(fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis),)
+              ],
+            ),
+          );
+         },
+        ),
     );
   }
 }
@@ -137,7 +193,7 @@ class _CustomSliverAppBar extends StatelessWidget {
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        title: Text(movie.title, style: const TextStyle(fontSize: 20), textAlign: TextAlign.start,),
+        //title: Text(movie.title, style: const TextStyle(fontSize: 20), textAlign: TextAlign.start,),
         background: Stack(
           children: [
             SizedBox.expand(

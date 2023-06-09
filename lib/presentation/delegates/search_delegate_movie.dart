147cfafe45d83a79870130980e409d6df2b86328
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-06-09 12:36:46 -0500

    Mostrar calificacion de la pelicula

--- lib/presentation/delegates/search_delegate_movie.dart
+++ lib/presentation/delegates/search_delegate_movie.dart
@@ -1,5 +1,6 @@
 
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:cinemapedia/domain/entities/movie_entity.dart';
 import 'package:flutter/material.dart';
 
@@ -97,6 +98,16 @@ class _MovieItem extends StatelessWidget {
                 movie.overview.length > 100
                   ? Text('${movie.overview.substring(0,100)}...')
                   : Text(movie.overview),
+
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800,),
+                    const SizedBox(width: 5,),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),)
+                  ],
+                )
               ],
             ),
           )

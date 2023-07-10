f017785947e6a8be63703822882f6d663f643dcf
Diego Fernando Hoyos Chacon <diego.hoyos@wiedii.co>
2023-07-10 09:58:02 -0500

    Boton para marcar como favorito

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -194,6 +194,12 @@ class _CustomSliverAppBar extends StatelessWidget {
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+          onPressed: (){},
+          icon: const Icon(Icons.favorite_border))
+          //icon: Icon(Icons.favorite, color: Colors.red,))
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         //title: Text(movie.title, style: const TextStyle(fontSize: 20), textAlign: TextAlign.start,),
@@ -209,38 +215,60 @@ class _CustomSliverAppBar extends StatelessWidget {
                 },
                 ),
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    stops: [0.7, 1.0],
-                    colors: [
-                    Colors.transparent,
-                    Colors.black87
-                  ])
-                )
-                ),
+            const _CustomGradient(
+              begin: Alignment.topCenter, 
+              end: Alignment.bottomCenter,  
+              stops: [0.8, 1.0],
+              colors: [Colors.transparent, Colors.black87]
             ),
-            const SizedBox.expand(
-              child: DecoratedBox(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    stops: [0.0, 0.4],
-                    colors: [
-                    Colors.black87,
-                    Colors.transparent
-                  ])
-                )
-                ),
-            )
+            const _CustomGradient(
+              begin: Alignment.topLeft, 
+              end: Alignment.bottomRight,  
+              stops: [0.0, 0.4],
+              colors: [Colors.black87,Colors.transparent]
+            ),
+            const _CustomGradient(
+              begin: Alignment.topRight, 
+              end: Alignment.bottomLeft,  
+              stops: [0.0, 0.2],
+              colors: [Colors.black54,Colors.transparent]
+            ),
           ],
         ),
       ),
       
     );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+
+  const _CustomGradient({
+    required this.begin, 
+    required this.end, 
+    required this.stops, 
+    required this.colors
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: begin,
+                    end: end,
+                    stops: stops,
+                    colors: colors)
+                )
+                ),
+            );
   }
 }
 
